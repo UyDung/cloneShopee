ab37b8ae1b070f87e4cfa6073afda29280215d26
UyDung <dungnguyendinh911@gmail.com>
2021-12-21 17:17:11 +0700

    shopee live, fix swiper-pagination position

--- js/swiper.js
+++ js/swiper.js
@@ -1,30 +1,68 @@
 const navSwiper = new Swiper(".nav-swiper", {
-    speed: 500,
+    speed: 700,
     slidesPerView: 1,
     autoplay: {
         delay: 4000,
-    },
+    }, 
     pagination: {
-        el: "#swiper-pagination-1",
-    },
+        el: ".swiper-pagination-1",
+        type: "bullets",
+        clickable: true,
+    }, 
+    loop: true, 
+    slideToClickedSlide: true,
 });
 
 const sloganSwiper = new Swiper(".slogan-swiper", {
     autoplay: false,
+    speed: 100,
     grid: {
         rows: 2,
         fill: "column",
     },
-    slidesPerView: 4,
+    slidesPerView: "auto",
+    pagination: {
+        el: ".swiper-pagination-slogan",
+        type: "bullets",
+        clickable: true,
+    },
 });
 
 const flashSwiper = new Swiper(".product-container", {
-    autoplay: false,    
-    slidesPerView: 3,
+    autoplay: false,
+    slidesPerView: "auto",
+    loppedSlides: 11,
+    speed: 300
 });
-
 
 const topSearchSwiper = new Swiper(".top-search-container", {
     autoplay: false,
-     slidesPerView: 3,
+    slidesPerView: "auto",    
 });
+
+const shopeeMallSwiper = new Swiper(".shopee-mall-swiper", {
+    autoplay: {
+        delay: 2000,
+    },
+
+    clickable: true,
+    slidesPerView: 1,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination-shopee",
+        type: "bullets",
+        clickable: true,
+    },
+});
+
+const shopeeMallProducts = new Swiper(".shopee-mall-products", {
+    
+    slidesPerView: 'auto',
+    loopedSlides: 3,
+    spaceBetween: 10
+});
+
+const shopeeLiveItems = new Swiper(".shopee-live-list", {
+    slidesPerView: 'auto',
+    
+})
